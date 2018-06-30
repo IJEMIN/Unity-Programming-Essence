d2a55a810fad18024ec304a96d9399da5a6b73b3
i_jemin <i_jemin@hotmail.com>
2018-07-01 03:43:33 +0900

    Re-organize material assets

--- 07/Dodge/Assets/PlayerController.cs
+++ 07/Dodge/Assets/PlayerController.cs
@@ -2,30 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     private Rigidbody playerRigidbody;
     public float speed = 8f;
 
-    void Start()
-    {
-        playerRigidbody = GetComponent<Rigidbody>();
+    void Start () {
+        playerRigidbody = GetComponent<Rigidbody> ();
     }
 
-    void Update()
-    {
-        float xInput = Input.GetAxis("Horizontal");
-        float zInput = Input.GetAxis("Vertical");
+    void Update () {
+        float xInput = Input.GetAxis ("Horizontal");
+        float zInput = Input.GetAxis ("Vertical");
 
         float xSpeed = xInput * speed;
         float zSpeed = zInput * speed;
 
-        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+        Vector3 newVelocity = new Vector3 (xSpeed, 0f, zSpeed);
         playerRigidbody.velocity = newVelocity;
     }
 
-    public void Die()
-    {
-        gameObject.SetActive(false);
+    public void Die () {
+        gameObject.SetActive (false);
     }
 }
