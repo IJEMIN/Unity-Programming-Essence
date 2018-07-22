af32f699fad4d544b1a789e4271dd6b4f17cb0b1
i_jemin <i_jemin@hotmail.com>
2018-07-22 22:06:31 +0900

    Apply roughly 14

--- 14/Zombie/Assets/Scripts/PlayerMovement.cs
+++ 14/Zombie/Assets/Scripts/PlayerMovement.cs
@@ -1,46 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-    public float moveSpeed = 3f;
-    public float rotateSpeed = 90f;
-    private PlayerInput playerInput;
-    private Rigidbody playerRigidbody;
-    private Animator playerAnimator;
-    void Start () {
-        playerInput = GetComponent<PlayerInput> ();
-        playerRigidbody = GetComponent<Rigidbody> ();
-        playerAnimator = GetComponent<Animator> ();
+    public float moveSpeed = 5f; // 앞뒤 움직임의 속도
+    public float rotateSpeed = 180f; // 좌우 회전 속도
+    private PlayerInput playerInput; // 플레이어의 입력을 전달하는 컴포넌트
+    private Rigidbody playerRigidbody; // 플레이어 자신의 리지드바디
+    private Animator playerAnimator; // 플레이어 자신의 애니메이터
+
+    void Start() {
+        // 사용할 컴포넌트들의 참조를 가져온다
     }
 
-    void OnDisable () {
-        playerRigidbody.isKinematic = true;
+    void Update() {
+        // 매 프레임 실행할 처리들이 온다
+
+        Rotate(); // 회전 실행
+        Move(); // 움직임 실행
     }
 
-    // Update is called once per frame
-    void Update () {
-        Rotate ();
-        Move ();
-
-        playerAnimator.SetFloat ("Move", playerInput.move);
-        playerAnimator.SetFloat ("Rotate", playerInput.rotate);
+    private void Move() {
+        // 입력값에 따라 캐릭터를 앞뒤로 움직인다
 
     }
 
-    private void Move () {
-        playerRigidbody.position += playerInput.move * transform.forward * moveSpeed * Time.deltaTime;
-    }
+    private void Rotate() {
+        // 입력값에 따라 캐릭터를 좌우로 회전한다
 
-    void Rotate () {
-        playerRigidbody.rotation *= Quaternion.Euler (0, playerInput.rotate * rotateSpeed * Time.deltaTime, 0f);
-    }
-
-    void OnTriggerEnter (Collider other) {
-        Item item = other.GetComponent<Item> ();
-
-        if (item != null) {
-            item.Use (gameObject);
-        }
     }
 }
