6b5dcd39857d75e9a4aed38f83388d2112bbb220
I_Jemin <i_jemin@hotmail.com>
2018-09-07 15:11:13 +0900

    Done 07

--- 06/Done/Dodge/Assets/PlayerController.cs
+++ 06/Done/Dodge/Assets/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody playerRigidbody; // 이동에 사용할 리지드바디 컴포넌트
-    public float speed = 8f; // 이동 속도
+    public float speed = 8f; // 이동 속력
 
     void Start() {
         // 자신의 게임 오브젝트에서 Rigidbody 컴포넌트를 찾아 playerRigidbody에 할당
@@ -16,13 +16,13 @@ public class PlayerController : MonoBehaviour {
         float xInput = Input.GetAxis("Horizontal");
         float zInput = Input.GetAxis("Vertical");
 
-        // 실제 이동 속도를 입력 값과 이동 속도를 곱해 결정
+        // 실제 이동 속도를 입력 값과 이동 속력을 통해 결정
         float xSpeed = xInput * speed;
         float zSpeed = zInput * speed;
 
-        // Vector3 속력을 (xSpeed, 0, zSpeed)으로 생성
+        // Vector3 속도를 (xSpeed, 0, zSpeed)으로 생성
         Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
-        // 리지드바디의 속력에 newVelocity를 할당
+        // 리지드바디의 속도에 newVelocity를 할당
         playerRigidbody.velocity = newVelocity;
     }
 
--- 07/Done/Dodge/Assets/BulletSpawner.cs
+++ 07/Done/Dodge/Assets/BulletSpawner.cs
@@ -3,29 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletSpawner : MonoBehaviour {
-	public GameObject bulletPrefab;
-	public float spawnRateMin = 0.5f;
-	public float spawnRateMax = 3f;
+    public GameObject bulletPrefab; // 생성할 총알의 원본 프리팹
+    public float spawnRateMin = 0.5f; // 최소 생성 주기
+    public float spawnRateMax = 3f; // 최대 생성 주기
 
-	private Transform target;
-	private float spawnRate;
-	private float timeAfterSpawn;
+    private Transform target; // 발사할 대상
+    private float spawnRate; // 생성 주기
+    private float timeAfterSpawn; // 최근 생성 시점에서 지난 시간
 
-	void Start () {
-		timeAfterSpawn = 0f;
-		spawnRate = Random.Range (spawnRateMin, spawnRateMax);
-		target = FindObjectOfType<PlayerController> ().transform;
-	}
-	void Update () {
-		timeAfterSpawn += Time.deltaTime;
+    void Start() {
+        // 최근 생성 이후의 누적 시간을 0으로 초기화
+        timeAfterSpawn = 0f;
+        // 총알 생성 간격을 spawnRateMin과 spawnRateMax 사이에서 랜덤 지정 
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        // 총알을 조준할 대상으로 PlayerController 컴포넌트를 가진 게임 오브젝트를 찾기
+        target = FindObjectOfType<PlayerController>().transform;
+    }
 
-		if (timeAfterSpawn >= spawnRate) {
-			timeAfterSpawn = 0f;
+    void Update() {
+        // timeAfterSpawn을 갱신
+        timeAfterSpawn += Time.deltaTime;
 
-			GameObject bullet = Instantiate (bulletPrefab, transform.position, transform.rotation);
-			bullet.transform.LookAt (target);
+        // 최근 생성 시점에서부터 누적된 시간이, 생성 주기보다 크거나 같다면
+        if (timeAfterSpawn >= spawnRate)
+        {
+            // 누적된 시간을 리셋
+            timeAfterSpawn = 0f;
 
-			spawnRate = Random.Range (spawnRateMin, spawnRateMax);
-		}
-	}
+            // bulletPrefab의 복제본을 transform.position 위치와 transform.rotation 회전으로 생성
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            // 생성된 bullet 게임 오브젝트의 정면 방향이 target을 향하도록 회전
+            bullet.transform.LookAt(target);
+
+            // 다음번 총알 생성 간격을 spawnRateMin과 spawnRateMax 사이에서 랜덤 지정 
+            spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        }
+    }
 }
