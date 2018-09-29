882d9a556c9602ee342082f785ecc4665835745b
I_Jemin <i_jemin@hotmail.com>
2018-09-30 05:32:04 +0900

    Fix 11 Comments

--- 11/Done/Uni-Run/Assets/Scripts/PlayerController.cs
+++ 11/Done/Uni-Run/Assets/Scripts/PlayerController.cs
@@ -2,67 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// PlayerController는 플레이어로서 플레이어 게임 오브젝트를 제어하는 역할을 가진다.
-// 사용자 입력을 감지하여 점프를 하고, 물리 충돌을 통해 장애물에 부딫쳤을때 사망 처리를 한다.
-// 또한 현재 바닥에 발이 닿아있는지 체크하며, 상황에 알맞는 효과음과 애니메이션을 재생한다.
+// PlayerController는 플레이어 캐릭터로서 Player 게임 오브젝트를 제어한다.
 public class PlayerController : MonoBehaviour {
-    public AudioClip deathClip; // 죽었을때 재생할 오디오 클립
-    public float jumpForce = 700f; // 위쪽으로 점프하는 힘
+    public AudioClip deathClip; // 사망시 재생할 오디오 클립
+    public float jumpForce = 700f; // 점프 힘
 
-    private bool isGrounded = false; // 플레이어가 바닥에 닿아있는지
-    private bool isDead = false; // 플레이어가 죽었는지
+    private bool isGrounded = false; // 바닥에 닿았는지 나타냄
+    private bool isDead = false; // 사망 상태
 
-    private Rigidbody2D playerRigidbody; // 플레이어의 리기드바디 컴포넌트, 점프하는 힘을 주기 위해 사용
-    private Animator animator; // 플레이어의 애니메이터 컴포넌트, 애니메이션을 재생하기 위해 사용
-    private AudioSource playerAudio; // 플레이어의 오디오소스 컴포넌트, 오디오를 재생하기 위해 사용
+    private Rigidbody2D playerRigidbody; // 사용할 리지드바디 컴포넌트
+    private Animator animator; // 사용할 애니메이터 컴포넌트
+    private AudioSource playerAudio; // 사용할 오디오 소스 컴포넌트
 
-    private void Start () {
-        // 초기화를 합니다
-        playerRigidbody = GetComponent<Rigidbody2D> ();
-        animator = GetComponent<Animator> ();
-        playerAudio = GetComponent<AudioSource> ();
+    private void Start() {
+        // 게임 오브젝트로부터 사용할 컴포넌트들을 가져와 변수에 할당
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
     }
 
-    private void Update () {
-        // 사용자 입력을 감지하고 점프를 하는 처리를 구현합니다
-        if (isDead) {
+    private void Update() {
+        if (isDead)
+        {
+            // 사망시 처리를 더이상 진행하지 않고 종료
             return;
         }
 
-        if (Input.GetMouseButtonDown (0) && isGrounded) {
-            playerRigidbody.AddForce (new Vector2 (0, jumpForce));
-            playerAudio.Play ();
-        } else if (Input.GetMouseButtonUp (0) && playerRigidbody.velocity.y > 0) {
+        // 바닥에 닿은 상태에서 마우스 왼쪽 버튼을 눌렀다면
+        if (isGrounded && Input.GetMouseButtonDown(0))
+        {
+            // 리지드바디에게 위쪽으로 힘을 주기
+            playerRigidbody.AddForce(new Vector2(0, jumpForce));
+            // 오디오 소스를 재생
+            playerAudio.Play();
+        }
+        else if (Input.GetMouseButtonUp(0) && playerRigidbody.velocity.y > 0)
+        {
+            // 마우스 왼쪽 버튼에서 손을 때는 순간 && 속도의 y값이 양수라면 (위로 상승중)
+            // 현재 속도를 절반으로 변경
             playerRigidbody.velocity = playerRigidbody.velocity * 0.5f;
         }
-        animator.SetBool ("Grounded", isGrounded);
+
+        // 애니메이터의 Grounded 파라미터를 isGrounded 값으로 갱신
+        animator.SetBool("Grounded", isGrounded);
     }
 
-    private void Die () {
-        // 플레이어가 사망했을 때의 처리를 구현합니다
-        animator.SetTrigger ("Die");
+    private void Die() {
+        // 애니메이터의 Die 트리거 파라미터를 셋
+        animator.SetTrigger("Die");
 
+        // 오디오 소스에 할당된 오디오 클립을 deathClip으로 변경
         playerAudio.clip = deathClip;
-        playerAudio.Play ();
+        // 사망 효과음 재생
+        playerAudio.Play();
 
+        // 속도를 (0, 0)으로 변경
         playerRigidbody.velocity = Vector2.zero;
+        // 사망 상태를 true로 변경
         isDead = true;
     }
 
-    private void OnTriggerEnter2D (Collider2D other) {
-        // 트리거 충돌체인 장애물에 부딫쳤을때의 처리가 옵니다
-        if (other.tag == "Dead" && !isDead) {
-            Die ();
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Dead" && !isDead)
+        {
+            // 충돌한 상대방의 태그가 Dead이며 아직 사망하지 않았다면 Die() 실행
+            Die();
         }
     }
 
-    private void OnCollisionEnter2D (Collision2D other) {
-        // 바닥에 닿았음을 감지하는 처리가 옵니다
+    private void OnCollisionEnter2D(Collision2D other) {
+        // 어떤 콜라이더와 닿은 경우 isGrounded를 true로 변경
         isGrounded = true;
     }
 
-    private void OnCollisionExit2D (Collision2D other) {
-        // 점프 등으로 바닥에서 떼어졌음을 감지하는 처리가 옵니다
+    private void OnCollisionExit2D(Collision2D other) {
+        // 어떤 콜라이더에서 떼어진 경우 isGrounded를 false로 변경
         isGrounded = false;
     }
 }
--- 11/Uni-Run/Assets/Scripts/PlayerController.cs
+++ 11/Uni-Run/Assets/Scripts/PlayerController.cs
@@ -2,47 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// PlayerController는 플레이어로서 플레이어 게임 오브젝트를 제어하는 역할을 가진다.
-// 사용자 입력을 감지하여 점프를 하고, 물리 충돌을 통해 장애물에 부딫쳤을때 사망 처리를 한다.
-// 또한 현재 바닥에 발이 닿아있는지 체크하며, 상황에 알맞는 효과음과 애니메이션을 재생한다.
+// PlayerController는 플레이어 캐릭터로서 Player 게임 오브젝트를 제어한다.
 public class PlayerController : MonoBehaviour {
-    public AudioClip deathClip; // 죽었을때 재생할 오디오 클립
-    public float jumpForce = 700f; // 위쪽으로 점프하는 힘
+   public AudioClip deathClip; // 사망시 재생할 오디오 클립
+   public float jumpForce = 700f; // 점프 힘
 
-    private bool isGrounded = false; // 플레이어가 바닥에 닿아있는지
-    private bool isDead = false; // 플레이어가 죽었는지
+   private bool isGrounded = false; // 바닥에 닿았는지 나타냄
+   private bool isDead = false; // 사망 상태
 
-    private Rigidbody2D playerRigidbody; // 플레이어의 리기드바디 컴포넌트, 점프하는 힘을 주기 위해 사용
-    private Animator animator; // 플레이어의 애니메이터 컴포넌트, 애니메이션을 재생하기 위해 사용
-    private AudioSource playerAudio; // 플레이어의 오디오소스 컴포넌트, 오디오를 재생하기 위해 사용
+   private Rigidbody2D playerRigidbody; // 사용할 리지드바디 컴포넌트
+   private Animator animator; // 사용할 애니메이터 컴포넌트
+   private AudioSource playerAudio; // 사용할 오디오 소스 컴포넌트
 
-    private void Start () {
-        // 초기화를 합니다
+   private void Start() {
+       // 초기화
+   }
 
-    }
 
-    private void Update () {
-        // 사용자 입력을 감지하고 점프를 하는 처리를 구현합니다
+   private void Update() {
+       // 사용자 입력을 감지하고 점프하는 처리
+   }
 
-    }
+   private void Die() {
+       // 사망 처리
+   }
 
-    private void Die () {
-        // 플레이어가 사망했을 때의 처리를 구현합니다
+   private void OnTriggerEnter2D(Collider2D other) {
+       // 트리거 콜라이더를 가진 장애물과의 충돌을 감지
+   }
 
-    }
+   private void OnCollisionEnter2D(Collision2D other) {
+       // 바닥에 닿았음을 감지하는 처리
+   }
 
-    private void OnTriggerEnter2D (Collider2D other) {
-        // 트리거 충돌체인 장애물에 부딫쳤을때의 처리가 옵니다
-
-    }
-
-    private void OnCollisionEnter2D (Collision2D other) {
-        // 바닥에 닿았음을 감지하는 처리가 옵니다
-
-    }
-
-    private void OnCollisionExit2D (Collision2D other) {
-        // 점프 등으로 바닥에서 떼어졌음을 감지하는 처리가 옵니다
-
-    }
+   private void OnCollisionExit2D(Collision2D other) {
+       // 바닥에서 벗어났음을 감지하는 처리
+   }
 }
