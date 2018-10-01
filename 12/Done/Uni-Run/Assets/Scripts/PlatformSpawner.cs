abc37758a0b7e7957299172df4cfdc786099419d
I_Jemin <i_jemin@hotmail.com>
2018-10-01 19:50:57 +0900

    Fix 12 Comments

--- 12/Done/Uni-Run/Assets/Scripts/PlatformSpawner.cs
+++ 12/Done/Uni-Run/Assets/Scripts/PlatformSpawner.cs
@@ -1,34 +1,30 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
-// 바닥을 주기적으로 배치하는 스크립트
-// 미리 바닥을 생성한 다음, 만들어진 바닥들을 돌아가며 사용하는 방식으로 무한 배치한다.
+// 발판을 생성하고 주기적으로 재배치하는 스크립트
 public class PlatformSpawner : MonoBehaviour {
+    public GameObject platformPrefab; // 생성할 발판의 원본 프리팹
+    public int count = 3; // 생성할 발판의 개수
 
-    public GameObject platformPrefab; // 생성할 바닥의 원본 프리팹
-    public int count = 3; // 게임이 시작될때 준비할 바닥 오브젝트의 갯수
+    public float timeBetSpawnMin = 1.25f; // 다음 배치까지의 시간 간격 최솟값
+    public float timeBetSpawnMax = 2.25f; // 다음 배치까지의 시간 간격 최댓값
+    private float timeBetSpawn; // 다음 배치까지의 시간 간격
 
-    public float timeBetSpawnMin = 1.25f; // 다음번 스폰까지의 시간 간격의 최솟값
-    public float timeBetSpawnMax = 2.25f; // 다음번 스폰까지의 시간 간격의 최댓값
-    private float timeBetSpawn; // 다음번 스폰까지의 시간 간격
+    public float yMin = -3.5f; // 배치할 위치의 최소 y값
+    public float yMax = 1.5f; // 배치할 위치의 최대 y값
+    private float xPos = 20f; // 배치할 위치의 x 값
 
-    public float yMin = -3.5f; // 스폰할 위치의 최소 y값
-    public float yMax = 1.5f; // 스폰할 위치의 최대 y값
-    private float xPos = 20f; // 스폰할 위치의 x 값
+    private GameObject[] platforms; // 미리 생성한 발판들
+    private int currentIndex = 0; // 사용할 현재 순번의 발판
 
-    private GameObject[] platforms; // 미리 생성된 바닥 모음
-    private int currentIndex = 0; // 현재 스폰할 바닥의 순번
+    private Vector2 poolPosition = new Vector2(0, -20); // 초반에 생성된 발판들을 화면 밖에 숨겨둘 위치
+    private float lastSpawnTime; // 마지막 배치 시점
 
-    private Vector2 poolPosition = new Vector2 (0, -20); // 처음 바닥들을 생성할때 화면 밖에 숨겨둘 위치
-    private float lastSpawnTime; // 마지막으로 스폰한 시점
 
-    void Start () {
-        // 변수들을 초기화하고 사용할 바닥 오브젝트들을 미리 생성한다
-
+    void Start() {
+        // 변수들을 초기화하고 사용할 발판들을 미리 생성
     }
 
-    void Update () {
-        // 순서를 돌아가며 바닥을 배치한다
-
+    void Update() {
+        // 순서를 돌아가며 주기적으로 발판을 배치
     }
 }
