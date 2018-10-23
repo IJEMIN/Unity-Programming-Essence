54674d44265eee5e68be81c9f919da02b61b2c74
I_Jemin <i_jemin@hotmail.com>
2018-10-23 15:21:07 +0900

    Refactoring && Commenting Done 18

---
+++ 18/Done/Zombie Multiplayer/Assets/Photon/PhotonUnityNetworking/Code/Editor/Views/PhotonTransformViewEditor.cs
@@ -0,0 +1,35 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="PhotonTransformViewEditor.cs" company="Exit Games GmbH">
+//   PhotonNetwork Framework for Unity - Copyright (C) 2018 Exit Games GmbH
+// </copyright>
+// <summary>
+//   This is a custom editor for the TransformView component.
+// </summary>
+// <author>developer@exitgames.com</author>
+// ----------------------------------------------------------------------------
+
+
+namespace Photon.Pun
+{
+    using UnityEditor;
+    using UnityEngine;
+
+    [CustomEditor(typeof(PhotonTransformView))]
+    public class PhotonTransformViewEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            if (Application.isPlaying)
+            {
+                EditorGUILayout.HelpBox("Editing is disabled in play mode.", MessageType.Info);
+                return;
+            }
+
+            PhotonTransformView view = (PhotonTransformView)target;
+
+            view.m_SynchronizePosition = PhotonGUI.ContainerHeaderToggle("Synchronize Position", view.m_SynchronizePosition);
+            view.m_SynchronizeRotation = PhotonGUI.ContainerHeaderToggle("Synchronize Rotation", view.m_SynchronizeRotation);
+            view.m_SynchronizeScale = PhotonGUI.ContainerHeaderToggle("Synchronize Scale", view.m_SynchronizeScale);
+        }
+    }
+}
---
+++ 18/Done/Zombie Multiplayer/Assets/Scripts/AmmoPack.cs
@@ -0,0 +1,22 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+// 총알을 충전하는 아이템
+public class AmmoPack : MonoBehaviourPun, IItem {
+    public int ammo = 30; // 충전할 총알 수
+
+    public void Use(GameObject target) {
+        // 전달 받은 게임 오브젝트로부터 PlayerShooter 컴포넌트를 가져오기 시도
+        PlayerShooter playerShooter = target.GetComponent<PlayerShooter>();
+
+        // PlayerShooter 컴포넌트가 있으며, 총 오브젝트가 존재하면
+        if (playerShooter != null && playerShooter.gun != null)
+        {
+            // 총의 남은 탄환 수를 ammo 만큼 더하기, 모든 클라이언트에서 실행
+            playerShooter.gun.photonView.RPC("AddAmmo", RpcTarget.All, ammo);
+        }
+
+        // 모든 클라이언트에서의 자신을 파괴
+        PhotonNetwork.Destroy(gameObject);
+    }
+}
