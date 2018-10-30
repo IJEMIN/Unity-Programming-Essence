a79def6bc8f10117a04af2ea48b5c3694ded827d
I_Jemin <i_jemin@hotmail.com>
2018-10-30 18:15:15 +0900

    Fix 19

--- 19/Zombie Multiplayer/Assets/Scripts/ColorSerialization.cs
+++ 19/Zombie Multiplayer/Assets/Scripts/ColorSerialization.cs
@@ -1,14 +1,21 @@
-﻿using UnityEngine;
-using WebSocketSharp;
+﻿using ExitGames.Client.Photon;
+using UnityEngine;
 
 public class ColorSerialization {
     public static byte[] SerializeColor(object targetObject) {
         Color color = (Color) targetObject;
-        return color.ToByteArray(ByteOrder.Big);
+
+        Quaternion colorToQuaterinon = new Quaternion(color.r, color.g, color.b, color.a);
+        byte[] bytes = Protocol.Serialize(colorToQuaterinon);
+
+        return bytes;
     }
 
     public static object DeserializeColor(byte[] bytes) {
-        Color color = Ext.To<Color>(bytes, ByteOrder.Big);
+        Quaternion quaterinon = (Quaternion) Protocol.Deserialize(bytes);
+
+        Color color = new Color(quaterinon.x, quaterinon.y, quaterinon.z, quaterinon.w);
+
         return color;
     }
 }
