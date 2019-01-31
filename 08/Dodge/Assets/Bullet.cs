01c0a509e1f84c5395d41cb7236ea18aac1fed1a
I_Jemin <i_jemin@hotmail.com>
2019-01-31 18:41:18 +0900

    CleanUp ch08 to Unity 2019.1b

--- 08/Dodge/Assets/Bullet.cs
+++ 08/Dodge/Assets/Bullet.cs
@@ -16,7 +16,7 @@ public class Bullet : MonoBehaviour {
         Destroy(gameObject, 3f);
     }
 
-    // 트리거 충돌시 자동으로 실행되는 메서드
+    // 트리거 충돌 시 자동으로 실행되는 메서드
     void OnTriggerEnter(Collider other) {
         // 충돌한 상대방 게임 오브젝트가 Player 태그를 가진 경우
         if (other.tag == "Player")
@@ -24,7 +24,7 @@ public class Bullet : MonoBehaviour {
             // 상대방 게임 오브젝트에서 PlayerController 컴포넌트를 가져오기
             PlayerController playerController = other.GetComponent<PlayerController>();
 
-            // 상대방으로부터 PlayerController 컴포넌트를 가져오는대 성공했다면
+            // 상대방으로부터 PlayerController 컴포넌트를 가져오는데 성공했다면
             if (playerController != null)
             {
                 // 상대방 PlayerController 컴포넌트의 Die() 메서드 실행
